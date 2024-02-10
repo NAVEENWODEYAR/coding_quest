5700edf378d26182e8464312f5e2bb1132346772
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-10 23:09:27 +0530

    initial commit

---
+++ src/main/java/com/collectors/CodingQuestApplication.java
@@ -0,0 +1,13 @@
+package com.collectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collectors/CodingQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collectors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
