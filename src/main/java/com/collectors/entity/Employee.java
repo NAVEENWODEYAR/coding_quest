068e7737e349b441d63c8696b82a9e6bc3eca120
Naveen <naveenkrishnawodeyar@gmail.com>
2024-04-24 23:32:47 +0530

    @EqualsAndHashCode

--- src/main/java/com/collectors/entity/Employee.java
+++ src/main/java/com/collectors/entity/Employee.java
@@ -3,10 +3,32 @@
  */
 package com.collectors.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 /**
  * @author LENOVO
  *
  */
-public class Employee {
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "CACHE_EMPLOYEE")
+public class Employee extends AbstractEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "EMP_ID")
+	private Long empId;
 
 }
