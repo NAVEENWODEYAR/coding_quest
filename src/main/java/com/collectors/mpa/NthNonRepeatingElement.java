7da371286450979d9ced38797fe8f171642fc595
Naveen <naveenkrishnawodeyar@gmail.com>
2024-07-05 21:48:55 +0530

    Unit test cases

--- src/main/java/com/collectors/mpa/NthNonRepeatingElement.java
+++ src/main/java/com/collectors/mpa/NthNonRepeatingElement.java
@@ -29,8 +29,7 @@ public class NthNonRepeatingElement {
 	}
 	
 	public static void main(String[] args) {
-        // int[] arr = {4, 5, 4, 5, 8, 3, 2, 1, 2, 3, 6}; 
-        int[] arr = {1,2,3,4,5,6,7,8,9,9,8,7,6,5,4,3,2,1}; // Example array
+        int[] arr = {1,2,3,4,5,6,7,8,9,9,8,7,6,5,4,3,2,1};
         System.out.println(nonRepatingElement(arr));
 
 	}
---
+++ src/test/java/com/collectors/RepeatingCharStringTest.java
@@ -0,0 +1,15 @@
+package com.collectors;
+
+import org.junit.jupiter.api.Test;
+
+import com.collectors.mpa.NthNonRepeatingElement;
+
+public class RepeatingCharStringTest {
+
+	NthNonRepeatingElement n = new NthNonRepeatingElement();
+	
+	@Test
+	public void nthTest() {
+		n = new NthNonRepeatingElement();
+	}
+}
