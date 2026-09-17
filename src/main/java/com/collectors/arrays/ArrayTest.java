7ccf335e78979578969b97fa301d410233f9ec55
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2026-09-17 21:24:42 +0530

    Update ArrayTest.java

--- src/main/java/com/collectors/arrays/ArrayTest.java
+++ src/main/java/com/collectors/arrays/ArrayTest.java
@@ -10,6 +10,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class ArrayTest {
 
 	public static void main(String[] args) {
+System.out.println("ArrayList");
 		List<Integer> list = new CopyOnWriteArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
 		list.forEach(System.out::print);
 		list.parallelStream().forEach(list::remove);
